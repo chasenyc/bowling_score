7b4d516387742aa8cbf5cb71be9b0299d65f2915
Alex Harris <chasenyc@gmail.com>
2016-03-08 16:36:56 -0500

    Refactor code, breaking out helper functions.

--- src/Bowling.js
+++ src/Bowling.js
@@ -1,85 +1,73 @@
 const score = (frames) => {
-
-  const frameCount = () => {
-    return frames.length
+  var sum = 0
+  for (i = 0; i < frames.length; i++) {
+    sum = sum + totalScore(frames, i)
   }
+  return sum
+}
 
-  const isStrike = (idx) => {
-    if (idx > frameCount() - 1) { return false }
-    return (frames[idx][0] === 10)
+const isStrike = (frames, idx) => {
+  if (idx > frames.length - 1) { return false }
+  return (frames[idx][0] === 10)
+}
+
+const isNextStrike = (frames, idx) => {
+  return (isStrike(frames, idx+1))
+}
+
+const isSpare = (frames, idx) => {
+  return ((frames[idx][0] + frames[idx][1] === 10) && !isStrike(frames, idx))
+}
+
+const frameScore = (frames, idx) => {
+  return frames[idx].reduce( (prev, curr) => prev + curr )
+}
+
+const nextTwoScores = (frames, idx) => {
+  return 10 + frames[idx + 2][0]
+}
+
+const totalScore = (frames, idx) => {
+  return frameScore(frames, idx) + calculateBonus(frames, idx)
+}
+
+const calculateBonus = (frames, idx) => {
+  if (isStrike(frames, idx)) {
+    return calcStrikeBonus(frames, idx)
+  } else if (isSpare(frames, idx)) {
+    return calcSpareBonus(frames, idx)
+  } else {
+    return 0
   }
+}
 
-  const isNextStrike = (idx) => {
-    return (isStrike(idx+1))
-  }
+const calcSpareBonus = (frames, idx) => {
+  return frames[idx+1][0]
+}
 
-  const isSpare = (idx) => {
-    return ((frames[idx][0] + frames[idx][1] === 10) && !isStrike(idx))
-  }
-
-  const frameFirst = (idx) => {
-    return frames[idx][0]
-  }
-
-  const frameSecond = (idx) => {
-    return frames[idx][1]
-  }
-
-  const frameScore = (idx) => {
-    return frames[idx].reduce( (prev, curr) => prev + curr )
-  }
-
-  const nextTwoScores = (idx) => {
-    return 10 + frames[idx + 2][0]
-  }
-
-  const totalScore = (idx) => {
-    var sum = 0
-    if (isStrike(idx) || isSpare(idx)) {
-      sum = sum + calculateBonus(idx)
-    }
-    return sum + frameScore(idx)
-  }
-
-  const calculateBonus = (idx) => {
-    return (isStrike(idx) ? calcStrikeBonus(idx) : calcSpareBonus(idx))
-  }
-
-  const calcSpareBonus = (idx) => {
-    return frames[idx+1][0]
-  }
-
-  const calcStrikeBonus = (idx) => {
-    if (isNextStrike(idx) && idx === 8) {
-      return 10 + frames[9][1]
+const calcStrikeBonus = (frames, idx) => {
+  if (isNextStrike(frames, idx) && idx === 8) {
+    return 10 + frames[9][1]
+  } else {
+    if (isNextStrike(frames, idx) && idx !== 8) {
+      return calcDoubleStrike(frames, idx)
     } else {
-      return ((isNextStrike(idx) && idx !== 8) ?
-               calcDoubleStrike(idx) : calcSingleStrike(idx))
+      return calcSingleStrike(frames, idx)
     }
   }
+}
 
-  const calcDoubleStrike = (idx) => {
-    return nextTwoScores(idx)
+const calcDoubleStrike = (frames, idx) => {
+  return nextTwoScores(frames, idx)
+}
+
+const calcSingleStrike = (frames, idx) => {
+  if (idx < frames.length - 1) {
+    return ((frames[idx+1].length > 1) ?
+             frames[idx+1][0] + frames[idx+1][1] : frames[idx+1][0])
+  } else {
+    return 0
   }
-
-  const calcSingleStrike = (idx) => {
-    if (idx < frameCount() - 1) {
-      return ((frames[idx+1].length > 1) ?
-               frames[idx+1][0] + frames[idx+1][1] : frames[idx+1][0])
-    } else {
-      return 0
-    }
-  }
-
-
-  const fullScore = () => {
-    var sum = 0
-    for (i = 0; i < frameCount(); i++) {
-      sum = sum + totalScore(i)
-    }
-    return sum
-  }
-  return fullScore()
 }
 
 // console.log(score([[2, 3], [3, 4], [7, 2]]))
